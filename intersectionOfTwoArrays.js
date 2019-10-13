ef60fecb70bb2375f884722dd90c4628f340d0ad
Diana Utan <didiutzi@gmail.com>
2019-10-13 11:14:07 -0500

    Update intersectionOfTwoArrays.js

--- intersectionOfTwoArrays.js
+++ intersectionOfTwoArrays.js
@@ -18,3 +18,25 @@ const intersect = (nums1, nums2) =>
     nums2.includes(item) ? nums2.splice(nums2.indexOf(item), 1): false
   ));
   
+  /** sorting array
+  var intersect = function(nums1, nums2){
+    let arr = [];
+    nums1.sort((a,b) => a - b);
+    nums2.sort((a,b) => a - b);
+    let i = 0; 
+    let j = 0;
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            arr.push(nums1[i]);
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
+        };
+    }; 
+    
+    return arr;
+};
+  **/ 
