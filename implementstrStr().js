3b37e2ec93c96edc1e953fd809ffd78101a8bec5
Diana Utan <didiutzi@gmail.com>
2019-10-27 12:13:48 -0500

    Update implementstrStr().js

--- implementstrStr().js
+++ implementstrStr().js
@@ -25,3 +25,15 @@ var strStr = function(haystack, needle) {
     }
     return -1
 };
+
+/** 
+let strStr = function(haystack, needle) {
+  if (!needle) return 0
+
+  for (let i = 0 ; i < haystack.length ; i++) {
+    if (haystack.slice(i, i+needle.length) === needle) return i
+  }
+
+  return -1
+}
+**/
