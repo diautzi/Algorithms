eb17a28adb8e72f61d5775be84e54646a90b230f
Diana Utan <didiutzi@gmail.com>
2019-11-02 17:03:49 -0500

    Create ghgs.js

---
+++ ghgs.js
@@ -0,0 +1,13 @@
+You are given an array of  integers, , and a positive integer, . Find and print the number of  pairs where  and  +  is divisible by .
+
+For example,  and . Our three pairs meeting the criteria are  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below. It should return the integer count of pairs meeting the criteria.
+
+divisibleSumPairs has the following parameter(s):
+
+n: the integer length of array 
+ar: an array of integers
+k: the integer to divide the pair sum by
