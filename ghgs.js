b647ae32150397858222c93d3bb2ed5c8fd357a6
Diana Utan <didiutzi@gmail.com>
2019-11-03 14:45:00 -0600

    Update ghgs.js

--- ghgs.js
+++ ghgs.js
@@ -11,3 +11,19 @@ divisibleSumPairs has the following parameter(s):
 n: the integer length of array 
 ar: an array of integers
 k: the integer to divide the pair sum by
+
+
+function divisibleSumPairs(n, k, ar) {
+    let count = 0;
+    for(let i=0; i<n; i++) {
+        for(let j=1; j<ar.length; j++) {
+            let sum = ar[i] + ar[j]
+            if(sum % k === 0) {
+                count+=1;
+                j+=1
+                console.log(sum)
+            }
+        }
+    }
+    return count
+}
