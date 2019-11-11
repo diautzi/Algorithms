326d3bf6ab4782900509257ec696396221b97f3e
Diana Utan <didiutzi@gmail.com>
2019-11-11 15:03:47 -0600

    Update euclidsAlgorithm.js

--- euclidsAlgorithm.js
+++ euclidsAlgorithm.js
@@ -1,3 +1,6 @@
+// the greatest common divisor (GCD) of two numbers
+// the largest number that divides both of them without leaving a remainder
+
 function gcd(a, b) {
   while (b !=0 ) {
     let temp = a;
