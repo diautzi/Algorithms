09f7257ace5e758272ddeac6beea3e400cfc1d05
Diana Utan <didiutzi@gmail.com>
2020-02-05 23:11:25 -0600

    Update happyString.js

--- happyString.js
+++ happyString.js
@@ -1,9 +1,6 @@
 // Write an algorithm to determine if a number is "happy".
-
 // A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
-
 // Example: 
-
 // Input: 19
 // Output: true
 // Explanation: 
@@ -14,9 +11,9 @@
 
 
 var isHappy = function(n) {
-    while(n !== 1) {
-// if(n === 4) return false;
-n = n.toString().split('').map(a => a*a).reduce((x, y) => (x + y))
-}
-return true;
+  while ( n !== 1 ) {
+    if ( n === 4 ) return false;
+    n = n.toString().split('').map(a => a*a).reduce((x, y) => (x + y))
+   }
+ return true;
 };
