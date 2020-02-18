c1137899839e914c7be1179cb7ff58e2b8db2bc1
Diana Utan <didiutzi@gmail.com>
2020-02-17 22:42:32 -0600

    Create searchInsertPosition.js

---
+++ searchInsertPosition.js
@@ -0,0 +1,15 @@
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+Example:
+ Input: [1,3,5,6], 5
+ Output: 2
+
+
+var searchInsert = function(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] >= target){
+            return i;
+        }
+    }
+    return nums.length;
+};
