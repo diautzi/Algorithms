43582f7a12045914b932d0a7e5ab2895e7d9907f
Diana Utan <didiutzi@gmail.com>
2020-05-08 11:53:54 -0500

    Update validAnagram.js

--- validAnagram.js
+++ validAnagram.js
@@ -1,15 +1,13 @@
-Given two strings s and t , write a function to determine if t is an anagram of s.
-
-Example 1:
-
-Input: s = "anagram", t = "nagaram"
-Output: true
-Example 2:
-
-Input: s = "rat", t = "car"
-Output: false
-Note:
-You may assume the string contains only lowercase alphabets.
+//Given two strings s and t , write a function to determine if t is an anagram of s.
+//
+//Example 1:
+//Input: s = "anagram", t = "nagaram"
+//Output: true
+//
+//Example 2:
+//Input: s = "rat", t = "car"
+//Output: false
+//Note: You may assume the string contains only lowercase alphabets.
 
 var isAnagram = function(s, t) {
     if (s.length === t.length === 0) return true;
